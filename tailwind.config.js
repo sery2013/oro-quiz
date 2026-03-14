8ac086fbdbda05e413b645997b8cbc83be0bee2d
SERY2013.ETH <101403669+sery2013@users.noreply.github.com>
2026-03-14 22:15:55 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,8 @@ export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {
-      colors: {
-        oro: '#ffd21f',
-        dark: '#0a0a0a',
-        turquoise: '#2F6F7C',
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
       },
     },
   },
